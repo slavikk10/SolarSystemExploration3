3e551dd681ae7b58540ddecd894ccb492afc476c
slavikbruh <slavikk10112012@gmail.com>
2025-11-02 14:41:02 +0200

    Added functionsupport.hpp, experimental planet collision

--- include/functionsupport.hpp
+++ include/functionsupport.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43cdd3c3d376ebe8b185e76f2000e121529a9bbdc6ff20b247e7d78956c73035
-size 1834
+oid sha256:d433aa7f87889968a72777cd190649d40b1d92fea4bf1d5f287d0dca77788744
+size 2655
--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26d14101085c4ec457d214f61be2d19e6c7641dc91d42e52b98721217a8dd59c
-size 106136
+oid sha256:b5bebb7f36e2e2b12e07e8d7bcc7f5ab96f03706f5e569dd1a697f854af77a47
+size 90118
