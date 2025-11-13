575f9a66e6f93216a99eec51272e1405d25cc350
slavikbruh <slavikk10112012@gmail.com>
2025-11-13 10:52:28 +0200

    Set capsule velocity to orbital velocity

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ad85e77d2eec279fc641416b4fca9e28e42ea4f3e94ccb311b2f70e77a5aa2
-size 91935
+oid sha256:ccba20828d0387aef3b20c1cbeb47267b5d6bdaf81553326cde60ff5f0ca16b8
+size 91938
