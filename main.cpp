db9c85b5854ff9a08a940928e292a50638857be9
slavikbruh <slavikk10112012@gmail.com>
2025-12-22 20:30:04 +0200

    Fixed lighting issues

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a47ed48e6a93bfc75b719d25c540c56412b38d3ab98328f7dca502789ccdb62
-size 92733
+oid sha256:6702ca6e07d44dcc6e758b08cd95a0cc2a8f82b8476aaf29fb2fb26cd926fafb
+size 92605
