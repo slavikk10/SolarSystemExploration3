ba315f544b429bdcabc2e3a88535aaf489b83c2b
slavikbruh <slavikk10112012@gmail.com>
2025-12-25 14:42:07 +0200

    Fixed non-alligned UI text, fixed player capsule not rendering

--- include/functionsupport.hpp
+++ include/functionsupport.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb5792a3b1e79d62fa7e240dc9760a13b87ce9bdd785dd7561dfee3cc43cec33
-size 3196
+oid sha256:cabcf6e517490d3e012b7b53938aff794c7b0321bff01370f5492b6ea8652258
+size 3183
