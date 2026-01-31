624a5618017dc95ebc969084c985e17f8270c85f
slavikbruh <slavikk10112012@gmail.com>
2026-01-31 15:59:51 +0200

    Implemented multi-threaded planet initialization

---
+++ include/threads.hpp
@@ -0,0 +1,56 @@
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t n) : stop(false) {
+        for (size_t i = 0; i < n; i++) {
+            threads.emplace_back([this] {
+                while (true) {
+                    std::function<void()> task;
+                    
+                    {
+                        std::unique_lock<std::mutex> lock(queue_mutex);
+                        cv.wait(lock, [this]{ return stop || !tasks.empty(); });
+
+                        if (stop && tasks.empty()) return;
+
+                        task = std::move(tasks.front());
+                        tasks.pop();
+                    }
+
+                    task();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            stop = true;
+        }
+        cv.notify_all();
+        for (auto &t : threads) t.join();
+    }
+
+    void enqueue(std::function<void()> task) {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            tasks.push(std::move(task));
+        }
+        cv.notify_one();
+    }
+
+private:
+    std::vector<std::thread> threads;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queue_mutex;
+    std::condition_variable cv;
+    bool stop;
+};
