73c429ee6d8dced17351ff6614211a61554cd92f
slavikbruh <slavikk10112012@gmail.com>
2026-02-01 18:32:22 +0200

    Verlet integration

---
+++ build/CMakeFiles/sse3.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vyacheslav/SSE3_OpenGL/src/glad.c" "CMakeFiles/sse3.dir/src/glad.c.o" "gcc" "CMakeFiles/sse3.dir/src/glad.c.o.d"
+  "/Users/vyacheslav/SSE3_OpenGL/main.cpp" "CMakeFiles/sse3.dir/main.cpp.o" "gcc" "CMakeFiles/sse3.dir/main.cpp.o.d"
+  "/Users/vyacheslav/SSE3_OpenGL/stb_image.cpp" "CMakeFiles/sse3.dir/stb_image.cpp.o" "gcc" "CMakeFiles/sse3.dir/stb_image.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
