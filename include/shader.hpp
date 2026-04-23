310e2d88019e411b61c51e1f8b0cc4177c70e2cd
slavikbruh <slavikk10112012@gmail.com>
2026-04-23 13:24:48 +0300

    LFS untracked include/

--- include/shader.hpp
+++ include/shader.hpp
@@ -1,3 +1,348 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1cdc3c610b668c6e254b7c322e3e401c3000f07b5f0cc96b879d102693d7a9bd
-size 11876
+//
+// Created by Vyacheslav on 30.03.2025.
+//
+
+#ifndef OPENGLTEST_SHADER_H
+#define OPENGLTEST_SHADER_H
+
+#include <glad/glad.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <glm/glm.hpp>
+
+class Shader {
+public:
+    unsigned int ID;
+
+    Shader(const char* vertexPath, const char* fragmentPath)
+    {
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+
+        vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        try {
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+
+            vShaderFile.close();
+            fShaderFile.close();
+
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        }
+        catch(std::ifstream::failure e) {
+            std::cout << "Error reading shader file.\n" << std::endl;
+        }
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        unsigned int vertex, fragment;
+        int success;
+        char infoLog[512];
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "Error compiling vertex shader. Info log:\n" <<
+                    infoLog << std::endl;
+        }
+
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "Error compiling fragment shader. Info log:\n" <<
+                      infoLog << std::endl;
+        }
+
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+
+        glGetProgramiv(ID, GL_LINK_STATUS, &success);
+        if(!success) {
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            std::cout << "Error linking shader program. Info log:\n" <<
+                    infoLog << std::endl;
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+
+    Shader(const char* vertexPath, const char* fragmentPath, const char* geomPath)
+    {
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::string geomCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+        std::ifstream gShaderFile;
+
+        vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        gShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        try {
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            gShaderFile.open(geomPath);
+            std::stringstream vShaderStream, fShaderStream, gShaderStream;
+
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            gShaderStream << gShaderFile.rdbuf();
+
+            vShaderFile.close();
+            fShaderFile.close();
+            gShaderFile.close();
+
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+            geomCode = gShaderStream.str();
+        }
+        catch(std::ifstream::failure e) {
+            std::cout << "Error reading shader file.\n" << std::endl;
+        }
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+        const char* gShaderCode = geomCode.c_str();
+
+        unsigned int vertex, fragment, geometry;
+        int success;
+        char infoLog[512];
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "Error compiling vertex shader. Info log:\n" <<
+                    infoLog << std::endl;
+        }
+
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "Error compiling fragment shader. Info log:\n" <<
+                      infoLog << std::endl;
+        }
+
+        geometry = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geometry, 1, &gShaderCode, NULL);
+        glCompileShader(geometry);
+
+        glGetShaderiv(geometry, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(geometry, 512, NULL, infoLog);
+            std::cout << "Error compiling geometry shader. Info log:\n" <<
+                      infoLog << std::endl;
+        }
+
+
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glAttachShader(ID, geometry);
+        glLinkProgram(ID);
+
+        glGetProgramiv(ID, GL_LINK_STATUS, &success);
+        if(!success) {
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            std::cout << "Error linking shader program. Info log:\n" <<
+                    infoLog << std::endl;
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+        glDeleteShader(geometry);
+    }
+
+    Shader(const char* vertexPath, const char* fragmentPath, const char* tescPath, const char* tesePath)
+    {
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::string tesscCode;
+        std::string tesseCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+        std::ifstream tcShaderFile;
+        std::ifstream teShaderFile;
+
+        vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        tcShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        teShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        try {
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            tcShaderFile.open(tescPath);
+            teShaderFile.open(tesePath);
+            std::stringstream vShaderStream, fShaderStream, tcShaderStream, teShaderStream;
+
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            tcShaderStream << tcShaderFile.rdbuf();
+            teShaderStream << teShaderFile.rdbuf();
+
+            vShaderFile.close();
+            fShaderFile.close();
+            tcShaderFile.close();
+            teShaderFile.close();
+
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+            tesscCode = tcShaderStream.str();
+            tesseCode = teShaderStream.str();
+        }
+        catch(std::ifstream::failure e) {
+            std::cout << "Error reading shader file.\n" << std::endl;
+        }
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+        const char* tcShaderCode = tesscCode.c_str();
+        const char* teShaderCode = tesseCode.c_str();
+
+        unsigned int vertex, fragment, tess_control, tess_eval;
+        int success;
+        char infoLog[512];
+
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "Error compiling vertex shader. Info log:\n" <<
+                    infoLog << std::endl;
+        }
+
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "Error compiling fragment shader. Info log:\n" <<
+                      infoLog << std::endl;
+        }
+
+        tess_control = glCreateShader(GL_TESS_CONTROL_SHADER);
+        glShaderSource(tess_control, 1, &tcShaderCode, NULL);
+        glCompileShader(tess_control);
+
+        glGetShaderiv(tess_control, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(tess_control, 512, NULL, infoLog);
+            std::cout << "Error compiling tessellation control shader. Info log:\n" <<
+                      infoLog << std::endl;
+        }
+
+        tess_eval = glCreateShader(GL_TESS_EVALUATION_SHADER);
+        glShaderSource(tess_eval, 1, &teShaderCode, NULL);
+        glCompileShader(tess_eval);
+
+        glGetShaderiv(tess_eval, GL_COMPILE_STATUS, &success);
+        if(!success) {
+            glGetShaderInfoLog(tess_eval, 512, NULL, infoLog);
+            std::cout << "Error compiling tessellation evaluation shader. Info log:\n" <<
+                      infoLog << std::endl;
+        }
+
+
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glAttachShader(ID, tess_control);
+        glAttachShader(ID, tess_eval);
+        glLinkProgram(ID);
+
+        glGetProgramiv(ID, GL_LINK_STATUS, &success);
+        if(!success) {
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            std::cout << "Error linking shader program. Info log:\n" <<
+                    infoLog << std::endl;
+        }
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+        glDeleteShader(tess_control);
+        glDeleteShader(tess_eval);
+    }
+
+    void use()
+    {
+        glUseProgram(ID);
+    }
+
+    void setBool(const std::string &name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+
+    void setInt(const std::string &name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setFloat(const std::string &name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setVec2(const std::string &name, glm::vec2 value) const
+    {
+        glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+
+    void setVec3(const std::string &name, glm::vec3 value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+
+    void setVec4(const std::string &name, const glm::vec4 &value) const
+    {
+        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+
+    void setMat2(const std::string &name, const glm::mat2 &mat) const
+    {
+        glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+    void setMat3(const std::string &name, const glm::mat3 &mat) const
+    {
+        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+
+    void setMat4(const std::string &name, const glm::mat4 &mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
+};
+
+#endif //OPENGLTEST_SHADER_H
--- include/socket.hpp
+++ include/socket.hpp
@@ -1,3 +1,185 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4b91d442410469ee1db7d70ed62a7b72826661226c4fe2054adca51277258849
-size 6295
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <thread>
+#include <unordered_map>
+
+#include <camera.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+extern std::vector<int> clientUIDs;
+
+std::vector<int> clients;
+std::mutex clients_mutex;
+
+extern Camera camera;
+
+extern int timestamp;
+
+extern std::unordered_map<int, glm::dvec3> playerPositions;
+extern std::mutex playerPositionsMutex;
+
+extern std::vector<glm::dvec3> planetPositions;
+extern std::vector<glm::dvec3> planetVelocities;
+
+// add client to the list that stores their sockets and to the list that stores their UID
+void addClient(int client_fd, int clientUID) 
+{
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    clients.push_back(client_fd);
+    //std::lock_guard<std::mutex> lockUID(clientUIDs_mutex);
+    //clientUIDs.push_back(clientUID);
+}
+
+// remove client from the list that stores their sockets and from the list that stores their UID
+void removeClient(int client_fd, int clientUID) 
+{
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    clients.erase(std::remove(clients.begin(), clients.end(), client_fd), clients.end());
+    //std::lock_guard<std::mutex> lockUID(clientUIDs_mutex);
+    //clientUIDs.erase(std::remove(clientUIDs.begin(), clientUIDs.end(), clientUID), clientUIDs.end());
+}
+
+// send message to all clients
+void broadcastMessage(const std::string& message, int sender_fd) 
+{
+    std::lock_guard<std::mutex> lock(clients_mutex);
+    for (int client_fd : clients) 
+    {
+        if (client_fd != sender_fd) 
+        {
+            send(client_fd, message.c_str(), message.length(), 0);
+        }
+    }
+}
+
+int containsPlanetData(std::string msg) 
+{
+    if (msg.rfind("SUN;",   0) == 0) return 0;
+    if (msg.rfind("EARTH;", 0) == 0) return 1;
+    if (msg.rfind("MOON;",  0) == 0) return 2;
+    return -1;
+}
+
+void handleClient(int client_fd, int uid=-1) 
+{
+    std::atomic<bool> clientRunning{true};
+    std::atomic<int> clientUID{uid};
+
+    addClient(client_fd, clientUID);
+    std::thread t1([&]() {
+        char buffer[8192];
+        while (clientRunning.load()) 
+        {
+            int bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+            if (bytes > 0) 
+            {
+                std::string msg(buffer, bytes);
+
+                std::vector<std::string> tokens;
+                std::stringstream ss(msg);
+                std::string item;
+
+                int senderUID;
+                double px, py, pz;
+
+                while (std::getline(ss, item, ';')) 
+                {
+                    if (!item.empty())
+                        tokens.push_back(item);
+                }
+
+                for (size_t i = 0; i < tokens.size();) 
+                {
+                    const std::string& token = tokens[i];
+
+                    if (token == "UID" && i + 1 < tokens.size()) 
+                    {
+                        clientUID = std::stoi(tokens[i + 1]);
+                        clientUIDs[clientUID] = clientUID;
+                        std::cout << "my UID is " << clientUID << "\n";
+                        i += 2;
+                    } 
+                    else if ((token == "SUN" || token == "EARTH" || token == "MOON") && i + 6 <= tokens.size()) 
+                    {
+                        int planetIndex = containsPlanetData(token + ";");
+
+                        double x = std::stof(tokens[i + 1]);
+                        double y = std::stof(tokens[i + 2]);
+                        double z = std::stof(tokens[i + 3]);
+                        double vx = std::stof(tokens[i + 4]);
+                        double vy = std::stof(tokens[i + 5]);
+                        double vz = std::stof(tokens[i + 6]);
+
+                        glm::dvec3 position = glm::dvec3(x, y, z);
+                        glm::dvec3 velocity = glm::dvec3(vx, vy, vz);
+
+                        std::cout << "Setting the goofy position to " << position.x << ", " << position.y << ", " << position.z << std::endl;
+                        std::cout << "Setting the goofy velocity to " << velocity.x << ", " << velocity.y << ", " << velocity.z << std::endl;
+
+                        planetPositions[planetIndex] = position;
+                        planetVelocities[planetIndex] = velocity;
+
+                        i += 7;
+                    }
+                    else if (token == "TIMESTAMP")
+                    {
+                        timestamp = std::stoi(tokens[i + 1]);
+                        i += 2;
+                    }
+                    else if (isdigit(msg[0]))
+                    {
+                        if (sscanf(msg.c_str(), "%i;%d;%d;%d;", &senderUID, &px, &py, &pz))
+                        {
+                            std::cout << "SENDER UID: " << senderUID << std::endl;
+                            std::lock_guard<std::mutex> lock(playerPositionsMutex);
+                            std::cout << "Setting player position " << px << ", " << py << ", " << pz << std::endl;
+                            playerPositions[senderUID] = glm::dvec3(px, py, pz);
+                        }
+
+                        i += 4;
+                    }
+                    else 
+                    {
+                        std::cerr << "Error: unknown argument: " << token << "\n";
+                        i++;
+                    }
+                }
+
+                broadcastMessage(msg, client_fd);
+            } 
+            else if (bytes == 0) 
+            {
+                clientRunning = false;
+                std::cout << "Player " << clientUID << " disconnected.\n";
+                removeClient(client_fd, clientUID);
+            }
+        }
+    });
+
+    std::thread t2([&]()
+    {
+        while (clientRunning.load()) 
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+            int currentUID = clientUID;
+
+            glm::dvec3 position = (glm::dvec3)camera.Position;
+
+            std::string msg = std::to_string(currentUID) + ";" + std::to_string(position.x) + ";" + std::to_string(position.y) + ";" + std::to_string(position.z) + ";";
+            broadcastMessage(msg, client_fd);
+        }
+    });
+
+    t1.join();
+    t2.join();
+
+    removeClient(client_fd, clientUID);
+    close(client_fd);
+}
